7865af58a8210dd17269c240f729340d8d8041fa
Parth Vora <parth.vora@blazeclan.com>
2023-12-19 12:48:43 +0530

    add

--- sandbox-accounts/terraform/s3.tf
+++ sandbox-accounts/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
+resource "aws_s3_bucket" "sandbox_account_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 }
 
